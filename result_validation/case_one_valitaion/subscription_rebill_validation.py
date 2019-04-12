f7ef1e9d1c74c95e405e11cd8766805918520051
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-04-12 14:46:35 +0300

    rebill validation starts

--- result_validation/case_one_valitaion/subscription_rebill_validation.py
+++ result_validation/case_one_valitaion/subscription_rebill_validation.py
@@ -58,7 +58,7 @@ else:
     print('external_message_id false')
 
 """Select gateway_id for validation after"""
-def select_gateway_id():
+def define_subscription_gateway_id():
     cursor = connection.cursor()
     gateway_select_query = "SELECT * " \
                            "FROM gateway_aliases " \
@@ -76,7 +76,7 @@ def subscription_gateway_id():
     return str(subscription_gateway_id)
 
 
-if select_gateway_id() == subscription_gateway_id():
+if define_subscription_gateway_id() == subscription_gateway_id():
     print('partner true')
 else:
     print('partner false')
@@ -120,7 +120,7 @@ def select_created_first_rebill():
 """Save selected subscription data into variable"""
 save_first_rebill = select_created_first_rebill()
 
-
+"""Select click_id and validate it"""
 def first_rebill_click_id():
     first_rebill_click_id = save_first_rebill[6]
     return str(first_rebill_click_id)
@@ -131,4 +131,66 @@ else:
     print('first_rebill_click_id false')
 
 
-# print(first_rebill_click_id())
+"""Select gateway_id for validation after"""
+def define_first_rebill_gateway_id():
+    cursor = connection.cursor()
+    gateway_select_query = "SELECT * " \
+                           "FROM gateway_aliases " \
+                           "WHERE alias = '{}'".format(case_1_first_rebill_params['partner'])
+    cursor.execute(gateway_select_query)
+    gateway_cortage = cursor.fetchall()
+    gateway_parameters = gateway_cortage[0]
+    gateway_id = gateway_parameters[2]
+    return str(gateway_id)
+
+def first_rebill_gateway_id():
+    return str(save_first_rebill[2])
+
+if  define_first_rebill_gateway_id() == first_rebill_gateway_id():
+    print('first_rebill_gateway_id true')
+else:
+    print('first_rebill_gateway_id false')
+
+
+"""Select first rebill external_message_id and validate it"""
+def first_rebill_external_message_id():
+    return save_first_rebill[10]
+
+if case_1_first_rebill_params['external_message_id'] == first_rebill_external_message_id():
+    print('first_rebill_external_message_id true')
+else:
+    print('first_rebill_external_message_id false')
+
+
+"""Validate first rebill external_subscription_id"""
+if case_1_first_rebill_params['external_subscription_id'] == subscription_external_subscription_id():
+    print('First rebill external_subscription_id true')
+else:
+    print('First rebill external_subscription_id false')
+
+
+"""Validate payout parameters"""
+def select_first_rebill_currency_rate():
+    cursor = connection.cursor()
+    select_currency_rate_query = "SELECT rate " \
+                                 "FROM currency " \
+                                 "WHERE code = '{}'".format(case_1_first_rebill_params['currency'])
+    cursor.execute(select_currency_rate_query)
+    rate_cortage = cursor.fetchall()
+    first_rebill_row = rate_cortage[0]
+    return first_rebill_row[0]
+
+
+def calculate_first_rebill_payout_using_currency():
+    calculate_payout = select_first_rebill_currency_rate() \
+                       * float(case_1_first_rebill_params['payout'])
+    return calculate_payout
+
+
+def select_user_base_coefficient():
+
+
+
+# if case_1_first_rebill_params['currency']:
+    # print("true")
+# print(first_rebill_external_message_id())
