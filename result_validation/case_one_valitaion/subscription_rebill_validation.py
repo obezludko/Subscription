0257712a69dfa02560cf0d3170259b4a2fd308ca
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-04-12 12:11:46 +0300

    rebill validation starts

--- result_validation/case_one_valitaion/subscription_rebill_validation.py
+++ result_validation/case_one_valitaion/subscription_rebill_validation.py
@@ -1,6 +1,5 @@
 import psycopg2
-from parameters.subscription.case_1.subsciption_params import case_1_subscription_params
-from action.case_one_subscription_rebill_cancel import case_one_subscription
+from action.case_one_subscription_rebill_cancel import case_one_subscription, case_one_first_rebill
 from connection.connection_variables import pg_user, \
     pg_password, \
     pg_host, \
@@ -8,6 +7,7 @@ from connection.connection_variables import pg_user, \
     pg_database
 from parameters.subscription.case_1.subsciption_params import used_click_for_subscription, \
     case_1_subscription_params
+from parameters.rebill.case_1.rebill_params import case_1_first_rebill_params
 
 """Connect to database using connection variables"""
 connection = psycopg2.connect(database=pg_database,
@@ -16,9 +16,11 @@ connection = psycopg2.connect(database=pg_database,
                               host=pg_host,
                               port=pg_port)
 
+case_one_subscription()               # action subscription makes here
+case_one_first_rebill()               # First rebill action makes here
+
 
 def select_created_subscription():
-    case_one_subscription()                     # action subscription makes here
     cursor = connection.cursor()
     subscription_select_query = 'SELECT * ' \
                                 'FROM subscription ' \
@@ -30,11 +32,12 @@ def select_created_subscription():
 
 
 """Save selected subscription data into variable"""
-subscription_parameters = select_created_subscription()
+save_subscription_parameters = select_created_subscription()
+
 
 """Select subscription_click_id and validate it after"""
 def subscription_click_id():
-    subscription_click_id = subscription_parameters[3]
+    subscription_click_id = save_subscription_parameters[3]
     return str(subscription_click_id)
 
 
@@ -45,7 +48,7 @@ else:
 
 """Select subscription_external_message_id and validate it after"""
 def subscription_external_message_id():
-    subscription_external_message_id = subscription_parameters[14]
+    subscription_external_message_id = save_subscription_parameters[14]
     return str(subscription_external_message_id)
 
 
@@ -59,7 +62,7 @@ def select_gateway_id():
     cursor = connection.cursor()
     gateway_select_query = "SELECT * " \
                            "FROM gateway_aliases " \
-                           "WHERE alias = '%s';" % case_1_subscription_params['partner']
+                           "WHERE alias = '{}'".format(case_1_subscription_params['partner'])
     cursor.execute(gateway_select_query)
     gateway_cortage = cursor.fetchall()
     gateway_parameters = gateway_cortage[0]
@@ -69,7 +72,7 @@ def select_gateway_id():
 
 """Select subscription_gateway_id and validate it using previous selecting"""
 def subscription_gateway_id():
-    subscription_gateway_id = subscription_parameters[17]
+    subscription_gateway_id = save_subscription_parameters[17]
     return str(subscription_gateway_id)
 
 
@@ -80,7 +83,7 @@ else:
 
 """Select subscription_external_subscription_id and validate it"""
 def subscription_external_subscription_id():
-    subscription_external_subscription_id = subscription_parameters[2]
+    subscription_external_subscription_id = save_subscription_parameters[2]
     return subscription_external_subscription_id
 
 
@@ -91,7 +94,7 @@ else:
 
 """Select extra_param and validate it"""
 def select_extra_param():
-    subscription_extra_param = subscription_parameters[15]
+    subscription_extra_param = save_subscription_parameters[15]
     return subscription_extra_param
 
 
@@ -99,3 +102,33 @@ if case_1_subscription_params['extra_param'] == select_extra_param():
     print('extra_param true')
 else:
     print('extra_param false')
+
+
+"""First rebill validation makes here"""
+def select_created_first_rebill():
+    cursor = connection.cursor()
+    first_rebill_select_query = "SELECT * " \
+                                "FROM rebill " \
+                                "WHERE click_id = {} AND external_message_id = '{}'".format(used_click_for_subscription,
+                                                                                          case_1_first_rebill_params[
+                                                                                              'external_message_id'])
+    cursor.execute(first_rebill_select_query)
+    first_rebill_cortage = cursor.fetchall()
+    first_rebill_row = first_rebill_cortage[0]
+    return first_rebill_row
+
+"""Save selected subscription data into variable"""
+save_first_rebill = select_created_first_rebill()
+
+
+def first_rebill_click_id():
+    first_rebill_click_id = save_first_rebill[6]
+    return str(first_rebill_click_id)
+
+if case_1_first_rebill_params['click_id'] == first_rebill_click_id():
+    print('first_rebill_click_id true')
+else:
+    print('first_rebill_click_id false')
+
+
+# print(first_rebill_click_id())
