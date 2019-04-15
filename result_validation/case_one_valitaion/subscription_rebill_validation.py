8032ba7c5d9580fbc360b7e1153a68314c5caa6d
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-04-15 16:49:04 +0300

    second and third rebills validation improvement

--- result_validation/case_one_valitaion/subscription_rebill_validation.py
+++ result_validation/case_one_valitaion/subscription_rebill_validation.py
@@ -3,7 +3,8 @@ import time
 from action.case_one_subscription_rebill_cancel import case_one_subscription,\
     case_one_first_rebill, \
     case_one_second_rebill, \
-    case_one_third_rebill
+    case_one_third_rebill, \
+    case_one_fourth_rebill
 from connection.connection_variables import pg_user, \
     pg_password, \
     pg_host, \
@@ -13,7 +14,7 @@ from parameters.subscription.case_1.subsciption_params import used_click_for_sub
     case_1_subscription_params
 from parameters.rebill.case_1.rebill_params import case_1_first_rebill_params, \
     case_1_second_rebill_params, \
-    case_1_third_rebill_params
+    case_1_third_rebill_params, case_1_fourth_rebill_params
 
 """Connect to database using connection variables"""
 connection = psycopg2.connect(database=pg_database,
@@ -26,6 +27,7 @@ case_one_subscription()               # Action subscription makes here
 case_one_first_rebill()               # First rebill action makes here
 case_one_second_rebill()              # Second rebill action makes here
 case_one_third_rebill()               # Third rebill action makes here
+case_one_fourth_rebill()              # Fourth rebill action makes here
 
 """Define sleeping time"""
 define_sleep = time.sleep(2)
@@ -266,6 +268,15 @@ if calculate_first_rebill_money_from_gateway_using_currency() == float(case_1_fi
 else:
     print('First rebill money_from_gateway false')
 
+
+"""Validate first rebill money_for partner"""
+if round(float(calculate_first_rebill_money_from_gateway_using_currency() \
+        * select_first_rebill_user_base_coefficient()),2) == round(float(save_first_rebill[5]),2):
+    print("first rebill money_for partner true")
+else:
+    print("first rebill money_for partner false")
+
+
 """Validate first_rebill is_payout_received"""
 def select_first_rebill_is_payout_received():
     cursor = connection.cursor()
@@ -287,9 +298,9 @@ def first_rebill_check_key_value():
 
 
 if select_first_rebill_is_payout_received() == first_rebill_check_key_value():
-    print('First rebill is_payout_received true')
+    print('First rebill is_payout_received = true. Test was passed')
 else:
-    print('First rebill is_payout_received false')
+    print("First rebill is_payout_received = false. Test wasn't passed")
 
 """Validate first_rebill extra_param"""
 if save_first_rebill[17] == case_1_first_rebill_params['extra_param']:
@@ -423,6 +434,15 @@ else:
     print('second rebill money_from_gateway false')
 
 
+"""Validate second rebill money_for partner"""
+if round(float(select_second_rebill_user_base_coefficient()
+               * calculate_second_rebill_money_from_gateway_using_currency()),2) \
+        == round(float(save_second_rebill[5]),2):
+    print("second rebill money_for partner true")
+else:
+    print("second rebill money_for partner false")
+
+
 """Validate second_rebill is_payout_received"""
 def select_second_rebill_is_payout_received():
     cursor = connection.cursor()
@@ -444,9 +464,9 @@ def second_rebill_check_key_value():
 
 
 if select_second_rebill_is_payout_received() == second_rebill_check_key_value():
-    print('second rebill is_payout_received true')
+    print("second rebill is_payout_received = true. Test was passed")
 else:
-    print('second rebill is_payout_received false')
+    print("second rebill is_payout_received = false. Test wasn't passed")
 
 
 """Validate second_rebill extra_param"""
@@ -476,7 +496,7 @@ else:
 """
 ========================================================================================
 ========================================================================================
-From this place we start validate second created rebill
+From this place we start validate third created rebill
 """
 
 
@@ -581,6 +601,15 @@ else:
     print('third rebill money_from_gateway false')
 
 
+"""Validate third rebill money_for partner"""
+if round(float(select_third_rebill_user_base_coefficient()
+               * calculate_third_rebill_money_from_gateway_using_currency()),2) \
+        == round(float(save_third_rebill[5]),2):
+    print("third rebill money_for partner true")
+else:
+    print("third rebill money_for partner false")
+
+
 """Validate third is_payout_received"""
 def select_third_rebill_is_payout_received():
     cursor = connection.cursor()
@@ -602,9 +631,9 @@ def third_rebill_check_key_value():
 
 
 if select_third_rebill_is_payout_received() == third_rebill_check_key_value():
-    print('third rebill is_payout_received true')
+    print("third rebill is_payout_received = true. Test was passed")
 else:
-    print('third rebill is_payout_received false')
+    print("third rebill is_payout_received = false. test wasn't passed")
 
 
 """Validate third_rebill extra_param"""
@@ -614,7 +643,7 @@ else:
     print('third rebill extra_param false')
 
 
-"""Validate second rebill user role"""
+"""Validate third rebill user role"""
 def select_third_rebill_user_role():
     cursor = connection.cursor()
     select_user_role = "SELECT role " \
@@ -630,3 +659,154 @@ if select_third_rebill_user_role() == save_third_rebill[18]:
 else:
     print('third rebill user_role false')
 
+
+"""
+========================================================================================
+========================================================================================
+From this place we start validate fourth created rebill
+"""
+
+
+def select_created_fourth_rebill():
+    cursor = connection.cursor()
+    fourth_rebill_select_query = "SELECT * " \
+                                "FROM rebill " \
+                                "WHERE click_id = {} AND external_message_id = '{}'".format(used_click_for_subscription,
+                                                                                            case_1_fourth_rebill_params[
+                                                                                                'text_uniq_lead'])
+    cursor.execute(fourth_rebill_select_query)
+    fourth_rebill_cortage = cursor.fetchall()
+    fourth_rebill_row = fourth_rebill_cortage[0]
+    return fourth_rebill_row
+
+
+"""Save selected fourth rebill data into variable"""
+save_fourth_rebill = select_created_fourth_rebill()
+
+
+"""Select fourth rebill click_id and validate it"""
+def fourth_rebill_click_id():
+    fourth_rebill_click_id = save_fourth_rebill[6]
+    return str(fourth_rebill_click_id)
+
+if case_1_fourth_rebill_params['click_id'] == fourth_rebill_click_id():
+    print('fourth_rebill_click_id true')
+else:
+    print('fourth_rebill_click_id false')
+
+
+
+"""Select fourth rebill gateway_id for validation after"""
+def define_fourth_rebill_gateway_id():
+    cursor = connection.cursor()
+    gateway_select_query = "SELECT * " \
+                           "FROM gateway_aliases " \
+                           "WHERE alias = '{}'".format(case_1_fourth_rebill_params['partner'])
+    cursor.execute(gateway_select_query)
+    gateway_cortage = cursor.fetchall()
+    gateway_parameters = gateway_cortage[0]
+    gateway_id = gateway_parameters[2]
+    return str(gateway_id)
+
+def fourth_rebill_gateway_id():
+    return str(save_fourth_rebill[2])
+
+if define_fourth_rebill_gateway_id() == fourth_rebill_gateway_id():
+    print('fourth_rebill_gateway_id true')
+else:
+    print('fourth_rebill_gateway_id false')
+
+
+"""Validate fourth_rebill external_message_id(text_uniq_lead)"""
+if case_1_fourth_rebill_params['text_uniq_lead'] == save_fourth_rebill[10]:
+    print('fourth_rebill_external_message_id true')
+else:
+    print('fourth_rebill_external_message_id false')
+
+
+"""Validate fourth rebill external_subscription_id"""
+if case_1_fourth_rebill_params['subscr'] == save_subscription_parameters[2]:
+    print('fourth rebill external_subscription_id true')
+else:
+    print('fourth rebill external_subscription_id false')
+
+
+"""Validate related with fourth rebill's money parameters"""
+def select_fourth_rebill_sms_offer_payout():
+    cursor = connection.cursor()
+    select_fourth_rebill_sms_offer_payout = "SELECT payout " \
+                                            "FROM sms_offer " \
+                                            "WHERE id = {}".format(save_fourth_rebill[9])
+    cursor.execute(select_fourth_rebill_sms_offer_payout)
+    fourth_rebill_sms_offer_payout_cortage = cursor.fetchall()
+    fourth_rebill_sms_offer_payout = fourth_rebill_sms_offer_payout_cortage[0]
+    return fourth_rebill_sms_offer_payout[0]
+
+
+"""Define fourth rebill user base coefficient"""
+def select_fourth_rebill_user_base_coefficient():
+    cursor = connection.cursor()
+    select_user_base_coefficient = "SELECT base_coefficient " \
+                                   "FROM \"user\" " \
+                                   "WHERE id = {}".format(save_fourth_rebill[3])
+    cursor.execute(select_user_base_coefficient)
+    base_coefficient_cortage = cursor.fetchall()
+    base_coefficient = base_coefficient_cortage[0]
+    return base_coefficient[0]
+
+"""Calculate fourth rebill money_from_gateway"""
+def calculate_fourth_rebill_money_from_gateway():
+    calculate_payout = float(select_fourth_rebill_sms_offer_payout()) \
+                       * float(select_fourth_rebill_user_base_coefficient())
+    return calculate_payout
+
+
+"""Validate fourth rebill money_from_gateway"""
+if select_fourth_rebill_sms_offer_payout() == save_fourth_rebill[4]:
+    print("fourth rebill money_from_gateway true")
+else:
+    print("fourth rebill money_from_gateway false")
+
+
+"""Validate fourth rebill money_for_partner"""
+if save_fourth_rebill[5] == calculate_fourth_rebill_money_from_gateway():
+    print("fourth rebill money_for_partner true")
+else:
+    print("fourth rebill money_for_partner false")
+
+
+def fourth_rebill_check_key_value():
+    is_payout = 'payout' in case_1_fourth_rebill_params
+    return is_payout
+
+
+if save_fourth_rebill[16] == fourth_rebill_check_key_value():
+    print('Fourth rebill is_payout_received = false. Test was passed')
+else:
+    print("Fourth rebill is_payout_received = true. Test wasn't passed")
+
+
+
+"""Validate fourth_rebill extra_param"""
+if save_fourth_rebill[17] == case_1_fourth_rebill_params['extra_param']:
+    print('fourth rebill extra_param true')
+else:
+    print('fourth rebill extra_param false')
+
+
+"""Validate fourth rebill user role"""
+def select_fourth_rebill_user_role():
+    cursor = connection.cursor()
+    select_user_role = "SELECT role " \
+                       "FROM \"user\" " \
+                       "WHERE id = {}".format(save_fourth_rebill[3])
+    cursor.execute(select_user_role)
+    user_role_cortage = cursor.fetchall()
+    user_role = user_role_cortage[0]
+    return user_role[0]
+
+
+if select_fourth_rebill_user_role() == save_fourth_rebill[18]:
+    print('fourth rebill user_role true')
+else:
+    print('fourth rebill user_role false')
