5ce9c7b7a91c6ea2ef9593280630f926a7ca0de2
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-04-17 11:55:05 +0300

    fourth rebill validation and unsubscription improvements

--- parameters/subscription/case_2/subscription_params.py
+++ parameters/subscription/case_2/subscription_params.py
@@ -13,35 +13,37 @@ case_2_first_subscription_params = {'partner': 'test',
                                     'extra_param': 'subTWO123'}
 
 case_2_second_subscription_params = {'partner': 'test',
-                                    'action': 'SUBSCRIBE',
-                                    'click_id': '',                                                      # click id field is required
-                                    'external_message_id': used_click_for_subscription + 'q2',
-                                    'external_subscription_id': used_click_for_subscription + 'w2'}
+                                     'action': 'SUBSCRIBE',
+                                     'click_id': '',  # click id field is required
+                                     'external_message_id': used_click_for_subscription + 'q2',
+                                     'external_subscription_id': used_click_for_subscription + 'w2'}
 
 case_2_third_subscription_params = {'partner': 'test',
-                                    'action': 'SUBSCRIBE',
-                                    # without click_id parameter
+                                    'action': 'UNKNOWN', # UNKNOWN action
+                                    'click_id': used_click_for_subscription,
                                     'external_message_id': used_click_for_subscription + 'q2',
                                     'external_subscription_id': used_click_for_subscription + 'w2'}
 
 case_2_fourth_subscription_params = {'partner': 'eboy',
-                                    'action':'subscribe',
-                                    'click_id':used_click_for_subscription,
-                                    'text_uniq_lead':used_click_for_subscription + 'q3',
-                                    'subscr':used_click_for_subscription + 'w3',
-                                    'payout':'0,1'                                                       # the payout must be a number.
+                                     'action': 'subscribe',
+                                     'click_id': used_click_for_subscription,
+                                     'text_uniq_lead': used_click_for_subscription + 'q3',
+                                     'subscr': used_click_for_subscription + 'w3',
+                                     'payout': '0,1'  # the payout must be a number.
+                                     }
+
+case_2_fifth_subscription_params = {'partner': 'alavala666',  # partner don't exist
+                                    'action': 'init',
+                                    'click_id': used_click_for_subscription,
+                                    'external_message_id': used_click_for_subscription + 'q4',
+                                    'subscr': used_click_for_subscription + 'w4'
                                     }
 
-case_2_fifth_subscription_params = {'partner':'alavala666',                                              # partner don't exist
-                                     'action':'init',
-                                     'click_id':used_click_for_subscription,
-                                     'external_message_id':used_click_for_subscription + 'q4',
-                                     'subscr':used_click_for_subscription + 'w4'
-                                     }
+case_2_sixth_subscription_params = {'partner': '',  # blank partner parameter
+                                    'action': 'init',
+                                    'click_id': used_click_for_subscription,
+                                    'external_message_id': used_click_for_subscription + 'q5',
+                                    'subscr': used_click_for_subscription + 'w5'
+                                    }
 
-case_2_sixth_subscription_params = {'partner': '',                                                       # blank partner parameter
-                                     'action':'init',
-                                     'click_id':used_click_for_subscription,
-                                     'external_message_id':used_click_for_subscription + 'q5',
-                                     'subscr':used_click_for_subscription + 'w5'
-                                     }
+case_2_seventh_subscription_params = {}
