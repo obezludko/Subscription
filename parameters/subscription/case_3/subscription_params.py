f0908cfc958ede1081f864280be63d3811124128
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-04-18 15:21:43 +0300

    third case running and validation

--- parameters/subscription/case_3/subscription_params.py
+++ parameters/subscription/case_3/subscription_params.py
@@ -4,76 +4,87 @@ from click_sub.click import parse_url_and_append_click
 
 used_click_for_subscription = parse_url_and_append_click()
 
-case_3_first_subscription_params = {'partner':'test',
-                                    'action':'init',
+case_3_first_subscription_params = {'partner': 'test',
+                                    'action': 'init',
                                     'click_id': used_click_for_subscription,
                                     'external_message_id': used_click_for_subscription + 'q1',
                                     'external_subscription_id': used_click_for_subscription + 'w1',
-                                    'currency':'ALL',
-                                    'pauout':'1.5',
-                                    'extra_param':'case 3 SUB'
+                                    'currency': 'ALL',
+                                    'pauout': '1.5',
+                                    'extra_param': 'case 3 SUB'
                                     }
 
-
-case_3_first_rebill_params = {'partner':'test',
-                              'action':'rebill',
-                              'click_id':used_click_for_subscription,
-                              'external_message_id':used_click_for_subscription + 'q2',
-                              'external_subscription_id':used_click_for_subscription + 'w1',
-                              'currency':'ALL',
-                              'payout':'1,5',  # Payout must be a number
-                              'extra_param':'REBill 1'
+case_3_first_rebill_params = {'partner': 'test',
+                              'action': 'rebill',
+                              'click_id': used_click_for_subscription,
+                              'external_message_id': used_click_for_subscription + 'q2',
+                              'external_subscription_id': used_click_for_subscription + 'w1',
+                              'currency': 'ALL',
+                              'payout': '1,5',  # Payout must be a number
+                              'extra_param': 'REBill 1'
                               }
 
+case_3_second_rebill_params = {'partner': 'test',
+                               'action': 'rebill',
+                               'click_id': used_click_for_subscription,
+                               'external_message_id': used_click_for_subscription + 'q3',
+                               'external_subscription_id': used_click_for_subscription + 'w1',
+                               'currency': 'all',  # Writed in lowercase currency hasn't pass
+                               'payout': '1',
+                               'extra_param': 'REBill 2'
+                               }
 
-case_3_second_rebill_params = {'partner':'test',
-                              'action':'rebill',
-                              'click_id':used_click_for_subscription,
-                              'external_message_id':used_click_for_subscription + 'q3',
-                              'external_subscription_id':used_click_for_subscription + 'w1',
-                              'currency':'all',  # Writed in lowercase currency hasn't pass
-                              'payout':'1',
-                              'extra_param':'REBill 2'
+case_3_third_rebill_params = {'partner': 'test',
+                              'action': 'rebill',
+                              'click_id': used_click_for_subscription,
+                              'text_uniq_lead': used_click_for_subscription + 'q1',  # not unique external_message_id
+                              'subscr': used_click_for_subscription + 'w1',
+                              'extra_param': 'REBill 3'
                               }
 
+case_3_fourth_rebill_params = {'partner': 'test',
+                               'action': 'rebill',
+                               'click_id': used_click_for_subscription,
+                               'text_uniq_lead': used_click_for_subscription + 'q4',
+                               'subscr': used_click_for_subscription + 'w1111',
+                               # external_subscription_id doesn't exist
+                               'extra_param': 'REBill 4'
+                               }
 
-case_3_third_rebill_params = {'partner':'test',
-                              'action':'rebill',
-                              'click_id':used_click_for_subscription,
-                              'text_uniq_lead':used_click_for_subscription + 'q1',  # not unique external_message_id
-                              'subscr':used_click_for_subscription + 'w1',
-                              'extra_param':'REBill 3'
+case_3_fifth_rebill_params = {'partner': 'Alalo666',  # Partner doesn't exist
+                              'action': 'BILLING',
+                              'click_id': used_click_for_subscription,
+                              'external_message_id': used_click_for_subscription + 'q5',
+                              'subscr': used_click_for_subscription + 'w1',
+                              'extra_param': 'REBill 5'
                               }
 
-
-case_3_fourth_rebill_params = {'partner':'test',
-                              'action':'rebill',
-                              'click_id':used_click_for_subscription,
-                              'text_uniq_lead':used_click_for_subscription + 'q4',
-                              'subscr':used_click_for_subscription + 'w1111', # external_subscription_id doesn't exist
-                              'extra_param':'REBill 4'
-                              }
-
-
-case_3_fifth_rebill_params = {'partner':'Alalo666',  # Partner doesn't exist
-                              'action':'BILLING',
-                              'click_id':used_click_for_subscription,
-                              'external_message_id':used_click_for_subscription + 'q5',
-                              'subscr':used_click_for_subscription + 'w1',
-                              'extra_param':'REBill 5'
-                              }
-
-case_3_sixth_rebill_params = {'action': 'BILLING',    # Without partner parameter
+case_3_sixth_rebill_params = {'action': 'BILLING',  # Without partner parameter
                               'click_id': used_click_for_subscription,
                               'external_message_id': used_click_for_subscription + 'q6',
                               'subscr': used_click_for_subscription + 'w1',
                               'extra_param': 'REBill 6'
                               }
 
-case_3_seventh_rebill_params = {'partner':'test',
-                                'action':'UNKNOWN',  # UNKNOWN action
-                                'click_id':used_click_for_subscription,
-                                'external_message_id':used_click_for_subscription + 'q7',
-                                'subscr':used_click_for_subscription + 'w1',
-                                'extra_param':'REBill 7'
+case_3_seventh_rebill_params = {'partner': 'test',
+                                'action': 'UNKNOWN',  # UNKNOWN action
+                                'click_id': used_click_for_subscription,
+                                'external_message_id': used_click_for_subscription + 'q7',
+                                'subscr': used_click_for_subscription + 'w1',
+                                'extra_param': 'REBill 7'
                                 }
+
+case_3_eighth_rebill_params = {'partner': 'test',  # without action
+                               'click_id': used_click_for_subscription,
+                               'external_message_id': used_click_for_subscription + 'q8',
+                               'subscr': used_click_for_subscription + 'w1',
+                               'extra_param': 'REBill 8'
+                               }
+
+case_3_ninth_rebill_params = {'partner': 'test',
+                              'action': 'REBILL',
+                              'click_id': '',  # without click_id
+                              'external_message_id': used_click_for_subscription + 'q9',
+                              'subscr': used_click_for_subscription + 'w1',
+                              'extra_param': 'REBill 9'
+                              }
