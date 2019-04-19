e55d2b916dda6c07239f13f30bffb776120d8d8a
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-04-19 16:13:39 +0300

    fourth case running and validation

---
+++ parameters/rebill/case_4/rebill_params.py
@@ -0,0 +1,9 @@
+from parameters.subscription.case_4.subscription_params import used_click_for_subscription
+
+case_4_first_rebill_params = {'partner': 'test',
+                              'action': 'init',
+                              'click_id': used_click_for_subscription,
+                              'external_message_id': used_click_for_subscription + 'q1',
+                              'external_subscription_id': used_click_for_subscription + 'w1',
+                              'extra_param': ''
+                              }
