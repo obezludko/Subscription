f0fddb9f3f2b157a0c73b59e7c05123b495c9a1d
Oleg Bezludko <o.bezludko@abcmobile.com>
2019-05-10 15:34:00 +0300

    lead tests creating + variable lead improvement

--- action/lead_actions/case_one.py
+++ action/lead_actions/case_one.py
@@ -3,6 +3,36 @@ from connection.connection_variables import link_for_lead
 from parameters.lead.case_1.lead_params import *
 
 
-def case_one_lead():
+def case_one_first_lead():
     return requests.get(link_for_lead,case_1_first_lead_params)
+
+def case_one_second_lead():
+    return requests.get(link_for_lead,case_1_second_lead_params)
+
+def case_one_third_lead():
+    return requests.get(link_for_lead,case_1_third_lead_params)
+
+def case_one_fourth_lead():
+    return requests.get(link_for_lead,case_1_fourth_lead_params)
+
+def case_one_fifth_lead():
+    return requests.get(link_for_lead,case_1_fifth_lead_params)
+
+def case_one_sixth_lead():
+    return requests.get(link_for_lead,case_1_sixth_lead_params)
+
+def case_one_seventh_lead():
+    return requests.get(link_for_lead,case_1_seventh_lead_params)
+
+def case_one_eighth_lead():
+    return requests.get(link_for_lead,case_1_eighth_lead_params)
+
+def case_one_ninth_lead():
+    return requests.get(link_for_lead,case_1_ninth_lead_params)
+
+def case_one_tenth_lead():
+    return requests.get(link_for_lead,case_1_tenth_lead_params)
+
+def case_one_eleventh_lead():
+    return requests.get(link_for_lead,case_1_eleventh_lead_params)
 
--- parameters/lead/case_1/lead_params.py
+++ parameters/lead/case_1/lead_params.py
@@ -4,30 +4,48 @@ from click_sub.click import parse_url_and_append_click
  Lead hasn't be created"""
 
 used_click_for_lead = parse_url_and_append_click()
+# external_message_id = text_uniq_lead
+# external_subscription_id = subscr
 
-case_1_first_lead_params = {'partner':'',
-                            'click_id':'',
-                            'external_message_id':''}
+case_1_first_lead_params = {'partner': '',
+                            'click_id': '',
+                            'external_message_id': ''}
 
-case_2_first_lead_params = {}
+case_1_second_lead_params = {'partner': '',
+                             'click_id': '',
+                             'text_uniq_lead': ''}
 
-case_3_first_lead_params = {}
+case_1_third_lead_params = {'partner': 'test',
+                            'click_id': '',
+                            'external_message_id': ''}
 
-case_4_first_lead_params = {}
+case_1_fourth_lead_params = {'partner': '',
+                             'click_id': used_click_for_lead,
+                             'text_uniq_lead': ''}
 
-case_5_first_lead_params = {}
+case_1_fifth_lead_params = {'partner': '',
+                            'click_id': used_click_for_lead + 'abc',
+                            'external_message_id': ''}
 
-case_6_first_lead_params = {}
+case_1_sixth_lead_params = {'partner': '',
+                            'click_id': used_click_for_lead,
+                            'text_uniq_lead': used_click_for_lead + 'Q1'}
 
-case_7_first_lead_params = {}
+case_1_seventh_lead_params = {'partner': 'test',
+                              'click_id': used_click_for_lead,
+                              'text_uniq_lead': ''}
 
-case_8_first_lead_params = {}
+case_1_eighth_lead_params = {'partner': 'test',
+                             'click_id': '',
+                             'text_uniq_lead': used_click_for_lead + 'Q2'}
 
-case_9_first_lead_params = {}
+case_1_ninth_lead_params = {'click_id': used_click_for_lead,
+                            'text_uniq_lead':used_click_for_lead + 'Q3'}
 
-case_10_first_lead_params = {}
+case_1_tenth_lead_params = {'partner': 'test',
+                            'text_uniq_lead':used_click_for_lead + 'Q4'}
 
-case_11_first_lead_params = {}
+case_1_eleventh_lead_params = {'partner': 'test',
+                               'click_id': used_click_for_lead}
 
-case_12_first_lead_params = {}
 
